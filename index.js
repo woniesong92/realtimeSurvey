fd7fff9ebdda7ff19d8642b7a9ed44ce68cfb2aa
Howon Song <hs459@cornell.edu>
2014-11-12 21:02:42 -0500

    use a different channel everyday and scale the graphs if there are more than 100 votes for any model

--- index.js
+++ index.js
@@ -5,25 +5,40 @@ $(function() {
                 {name: "Kim", vote: 0},
                 {name: "Amy", vote: 0}];
 
+  var TODAY = new Date();
+  TODAY = "" + TODAY.getFullYear() + TODAY.getMonth() + TODAY.getDate();
+
   window.pubnub = PUBNUB.init({
-      channel: "survey",
+      channel: TODAY,
       publish_key: "pub-c-aa0da548-076b-4c46-bf87-87d2d457368a",
       subscribe_key: "sub-c-c3f6a9fe-45cb-11e4-a251-02ee2ddab7fe"
   });
 
   pubnub.subscribe({
-    channel: 'survey',
+    channel: TODAY,
     message: increment
   });
 
   function sendData(msg) {
     pubnub.publish({
-        channel: 'survey',
+        channel: TODAY,
         message: msg
     });
   }
 
+  function update_scale_if_many_votes(width_scale, data) {
+    for (var i=0; i<data.length; i++) {
+      var d = data[i];
+      if (d.vote >= 100) {
+        return (width_scale / (Math.floor(d.vote/100)+1));
+      }
+    }
+    return width_scale;
+  }
+
   function draw(data) {
+    var width_scale = 10;
+    width_scale = update_scale_if_many_votes(width_scale, data);
     var bars = d3.select(".container")
       .selectAll(".bar-wrapper")
       .data(data);
@@ -42,13 +57,13 @@ $(function() {
       .append("div")
       .attr("class", "bar")
       .style("width", function (d) {
-        return (d.vote*10)+15 + "px";
+        return (d.vote*width_scale)+15 + "px";
       })
       .text(function(d) { return d.vote });
     bars.selectAll("div")
       .text(function(d) { return d.vote })
       .style("width", function (d) {
-        return (d.vote*10)+15 + "px";
+        return (d.vote*width_scale)+15 + "px";
       });
     bars
       .exit()
@@ -67,7 +82,7 @@ $(function() {
 
   function init_votes() {
     pubnub.history({
-      channel: 'survey',
+      channel: TODAY,
       start: 0,
       callback: function(msg) {
         var vote_history = msg[0];
